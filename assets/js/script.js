90f8c4461bfc92ff99bc0c22339048c9f2880979
marycpriyanka <marycpriyanka@gmail.com>
2023-04-27 19:36:59 -0700

    Add basic styling to index.html

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const searchForm = document.getElementById("searchForm");
+const userSearchInput = document.getElementById("searchInput");
+const userFormatInput = document.getElementById("formatInput");
+
+function formSubmitHandler(event) {
+    event.preventDefault();
+
+    const query = 
+}
+
+searchForm.addEventListener("submit", formSubmitHandler);
+
